f49205ba4b222b30feaa05987ab7d513ae00093e
ahmedalirefa3y <ahmedalirefaey@gmail.con>
2014-11-07 20:55:28 +0200

    V1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace getproductsofallintsexceptatindex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getproductsofallintsexceptatindex(new[] { 1, 7, 3, 4 });
+        }
+
+        public static int[] getproductsofallintsexceptatindex(int[] a)
+        {
+            int[] b = new int[4];
+           
+            for (int i = 0; i < a.Length; i++)
+            {
+                int s = 1;
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        s *= a[j];
+                    }
+                }
+                b[i] = s;
+            }
+            return b;
+
+        }
+    }
+}
